670de9eedbe8070b7a6bd729a87be6ca5ea8260a
dev-kweku <frikasoidfrimpong@gmail.com>
2026-05-13 16:29:41 -1200

    jsonUtil added for data objects

---
+++ src/main/java/com/userapi/util/JsonUtil.java
@@ -0,0 +1,27 @@
+package com.userapi.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jaxrs.Jaxrs2TypesModule;
+
+import java.util.Map;
+
+public class JsonUtil {
+    private static final ObjectMapper mapper=new ObjectMapper()
+            .registerModule(new Jaxrs2TypesModule())
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+
+
+
+    public static String toJson(Object obj){
+        try{return mapper.writeValueAsString(obj);}catch(Exception e){return "{\"error\":\"Serialization failed\"}";}
+    }
+
+    public static <T> T fromJson(String json,Class<T> clazz) throws Exception{
+        return mapper.readValue(json,clazz);
+    }
+
+    public static  String error(String message){
+        return toJson(Map.of("error",message));
+    }
+}
