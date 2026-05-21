8374f8495cdd05b7d83e72525ef1ff83881951ab
dev-kweku <frikasoidfrimpong@gmail.com>
2026-05-20 14:37:14 -1200

    data seeder added

--- src/main/java/com/userapi/Main.java
+++ src/main/java/com/userapi/Main.java
@@ -1,7 +1,13 @@
 package com.userapi;
 
+import com.userapi.server.HttpServer;
+import com.userapi.server.Router;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Welcome to user api");
+    public static void main(String[] args) throws Exception {
+
+        var router=new Router();
+        new HttpServer(router,8080).start();
+        System.out.println("Server running on port http://localhost:8080");
     }
 }
--- src/main/java/com/userapi/seeder/DataSeeder.java
+++ src/main/java/com/userapi/seeder/DataSeeder.java
@@ -1,4 +1,33 @@
 package com.userapi.seeder;
 
+import javax.sql.DataSource;
+import java.util.Random;
+
 public class DataSeeder {
+    private static final int BATCH_SIZE=5_000;
+    private static final int THREAD_COUNT=8;
+
+    private final DataSource dataSource;
+    private final Random random=new Random();
+
+
+    private static final String[] FIRST_NAMES={};
+    private static final String[] LAST_NAME={};
+    private static final String[] CITIES={};
+    private static final String[] COUNTRIES={};
+    private static final String[] GENDERS={};
+
+    public DataSeeder(DataSource dataSource){
+        this.dataSource=dataSource;
+    }
+
+    public void seed(int total) throws Exception{
+
+    }
+
+    public void insertBatch() throws Exception{};
+
+    private String randomPhone(){
+        return String.format("+233%09d",random.nextInt(1_000_000_000));
+    }
 }
